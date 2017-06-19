e2ca59d04d0dc421d9392b775a63fb3236328028
JakubCzerny <kuba.czerny@poczta.fm>
2017-06-19 15:12:54 +0200

    tweak parameters

--- vehicle.py
+++ vehicle.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 """ This should not depend on anything else. This is a simple value type. """
 
@@ -7,18 +8,19 @@ class Vehicle:
     def __init__(self, lane, position=0.0):
         self.lane = lane
         self.position = position # meter
-        self.velocity = 33.0     # meter/sec
+        self.velocity = random.randint(15.0,30.0)     # meter/sec
         self.acceleration = 0.0  # meter/sec²
 
-        self.vd = 110
-        self.ds = 3
-        self.epsilon = 10
-        self.k  = 1
-        self.k1 = 1
-        self.k2 = 1
-        self.a0 = 1
-        self.l  = 1
-        self.a_max = 1
+        self.vd = random.randint(27.0,35.0)
+        # self.vd = 35.0 #m/s
+        self.ds = 6.0
+        self.epsilon = 5.0
+        self.k  = 1.5
+        self.k1 = 3.0
+        self.k2 = 2.0
+        self.a0 = 1.0
+        self.l  = 5.0
+        self.a_max = 1.0
 
     def __lt__(self, other):
         return isinstance(other, Vehicle) and self.position < other.position
@@ -29,24 +31,23 @@ class Vehicle:
 
     def prob_left(self, af, vf, df, dlf, vlf, dlb):
         p = 0
+
         if (self.vd - self.velocity > self.epsilon):
-            if not af:
-                p = 0
-            elif (af <= 0) \
-                and (not dlf or not (dlf < self.ds)) \
-                and (not dlb or not (dlb < self.ds)) \
-                and (not vlf or (not vlf > vf)):
-                p = (self.ds/df)**(3/4)  # P(left|state)
+            # and af <= 0 \
+            if af \
+                and (not dlf or (dlf > self.k*self.ds)) \
+                and (not dlb or (dlb > self.k*self.ds)):
+                # and (not vlf > vf):
+                p = (self.ds/df)#**(3/4)  # P(left|state)
 
         return p
 
     def prob_right(self, db, vrf, drf, drb):
         p = 0
         if (self.vd - self.velocity < self.epsilon) \
-            and (not drf or not (drf < self.k*self.ds)) \
-            and (not drb or not (drb < self.ds)) \
+            and (not drf or (drf > self.k*self.ds)) \
+            and (not drb or (drb > self.k*self.ds)) \
             and (not vrf or (self.velocity <= vrf)):
-
             if db:
                 p = np.sqrt(self.ds/db)  # P(right|state)
             else:
@@ -70,7 +71,7 @@ class Vehicle:
             if self.velocity > vf:
                 a = (vf - self.velocity) * self.l
             else:
-                if max(0, (self.vd - self.velocity)) == 0:
+                if max(0, (self.vd - self.velocity)) < self.epsilon:
                     a = 0
                 elif (self.vd - self.velocity) < self.epsilon:
                     a = self.a0
@@ -78,10 +79,13 @@ class Vehicle:
                     a = (self.vd - self.velocity) * self.l
         # braking zone
         else:
-            if self.velocity > vf:
-                a = -(-self.a_max / self.ds * df + self.a_max ) # always negative
+            if df < self.ds:
+                a = -3*self.a_max
+            elif self.velocity > vf:
+                a = (-self.a_max / self.ds * df + self.a_max ) # always negative
             else: # try these one at a time and see what works best!
-                a = 0 # option 1
+                # a = 0 # option 1
                 # a = small number # option 2
-                # a = -(-a_max / ds * df + a_max ) * (ds-df)/ds   # option 3
+                a = -0.5
+                # a = -(-self.a_max / self.ds * df + self.a_max ) * (self.ds-df)/self.ds   # option 3
         return a
