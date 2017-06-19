bd551ba4c0aa1ca89b23f493805128f4cc2fd2de
JakubCzerny <kuba.czerny@poczta.fm>
2017-06-19 12:54:22 +0200

    Simulation - first attempt

--- simulation.py
+++ simulation.py
@@ -24,7 +24,7 @@ class Simulation:
         self.try_spawn_vehicle()
 
     def time_step_vehicle(self, vehicle, dt):
-        # self.make_move(vehicle)
+        self.make_move(vehicle, dt)
         self.update_position(vehicle, dt)
 
         if vehicle.position > self._road_len:
@@ -33,8 +33,6 @@ class Simulation:
         else:
             print(vehicle)
 
-        # notify container if lane change!
-
     def update_position(self, vehicle, dt):
         vehicle.position += dt*vehicle.velocity \
                 + .5*dt*dt*vehicle.acceleration
@@ -42,49 +40,57 @@ class Simulation:
     def update_acceleration(self,vehicle,acc):
         vehicle.acceleration = acc
 
+    def update_velocity(self,vehicle,acc,dt):
+        vehicle.velocity += acc * dt
+
     def go_right(self,vehicle):
+        print(vehicle.lane, self._nb_lanes)
         vehicle.lane += 1
+        self._container.notify_lane_change(vehicle, vehicle.lane-1)
 
     def go_left(self,vehicle):
+        # print(vehicle.lane, self._nb_lanes)
         vehicle.lane -= 1
+        self._container.notify_lane_change(vehicle, vehicle.lane+1)
 
-    def make_move(self, vehicle):
+    def make_move(self, vehicle, dt):
         veh_f = self._container.front(vehicle)
-        af = veh_f.acceleration
-        vf = veh_f.velocity
-        df = veh_f.position  - vehicle.position
+        af = veh_f.acceleration if veh_f else None
+        vf = veh_f.velocity if veh_f else None
+        df = veh_f.position  - vehicle.position if veh_f else None
 
         veh_b = self._container.back(vehicle)
-        db = veh_b.position  - vehicle.position
+        db = veh_b.position  - vehicle.position if veh_b else None
 
         veh_lf = self._container.left_front(vehicle)
-        dlf = veh_lf.position - vehicle.position
+        dlf = veh_lf.position - vehicle.position if veh_lf else None
 
         veh_lb = self._container.left_back(vehicle)
-        dlb = veh_lb.position - vehicle.position
+        dlb = veh_lb.position - vehicle.position if veh_lb else None
 
         veh_rf = self._container.right_front(vehicle)
-        vrf = veh_rf.velocity
-        drf = veh_rf.position - vehicle.position
+        vrf = veh_rf.velocity if veh_rf else None
+        drf = veh_rf.position - vehicle.position if veh_rf else None
 
         veh_rb = self._container.right_back(vehicle)
-        drb = veh_rb.position - vehicle.position
+        drb = veh_rb.position - vehicle.position if veh_rb else None
 
         p = random.random()
-        p_right = vehicle.prob_right(af, db, vrf, drf, drb)
+        p_right = vehicle.prob_right(db, vrf, drf, drb)
 
-        if p > p_right:
+        if p > p_right and vehicle.lane+1 < self._nb_lanes:
             self.go_right(vehicle)
         else:
-            acc = vehicle.acceleration(vf, df)
+            acc = vehicle.calc_acceleration(vf, df)
 
             if acc != 0:
                 self.update_acceleration(vehicle, acc)
+                self.update_velocity(vehicle, acc, dt)
             else:
                 p = random.random()
                 p_left = vehicle.prob_left(af, vf, df, dlf, vlf, dlb)
 
-                if p > l_left:
+                if p > l_left and vehicle.lane > 0:
                     self.go_left(vehicle)
 
 
