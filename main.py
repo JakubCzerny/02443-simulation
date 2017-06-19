90062181f7ede83b46e680cb718af7295c26ea3b
JakubCzerny <kuba.czerny@poczta.fm>
2017-06-19 15:11:36 +0200

    Rescale the visualization

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from animation import Animation, AnimationInterrupt
 
 def start_sim():
     fps = 30
-    sim = Simulation(nb_lanes=3, road_len=300) # road len in meters
+    sim = Simulation(nb_lanes=3, road_len=350) # road len in meters
     anim = Animation(sim, fps)
 
     try:
@@ -21,4 +21,3 @@ def start_sim():
 
 if __name__ == "__main__":
     start_sim()
-
