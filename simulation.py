8c323c5e1828fbaf6ef15dba01a23a4f7633b81a
jakejhansen <jacobjonhansen@gmail.com>
2017-06-20 15:30:43 +0200

    Changed the spawn process to be from a poisson distribtuion with exponentially distributed arrival times

--- simulation.py
+++ simulation.py
@@ -8,6 +8,8 @@ class Simulation:
     def __init__(self, conf):
         self._conf = conf
         self._container = Container(conf.nb_lanes)
+        self._simulationTime = 0
+        self._tToNextVec = 0
 
     def time_step(self, dt):
         # loop over all vehicles, update all vehicles
@@ -15,7 +17,8 @@ class Simulation:
         for v in self:
             self.time_step_vehicle(v, dt)
 
-        self.try_spawn_vehicle()
+        self.try_spawn_vehicle2()
+        self._simulationTime += dt
 
     def time_step_vehicle(self, vehicle, dt):
         vehicle.update(self._conf, self._container, dt)
@@ -37,6 +40,25 @@ class Simulation:
                     self._conf.speed_range[0],    \
                     self._conf.speed_range[1])
             self._container.spawn(vehicle)
+            
+    def try_spawn_vehicle2(self):
+        if self._simulationTime >= self._tToNextVec:
+            lane = np.random.randint(self._conf.nb_lanes)
+
+            if self._container.last(lane):
+                last = self._container.last(lane)
+                if last.position < self._conf.safe_distance:
+                    return
+
+            vehicle = HumanVehicle(lane)
+            vehicle.velocity = np.random.uniform( \
+                    self._conf.speed_range[0],    \
+                    self._conf.speed_range[1])
+            self._container.spawn(vehicle)
+            
+            #Find time to next car
+            self._tToNextVec = self._simulationTime + \
+                    np.random.exponential(1/self._conf.spawn_rate)
 
     def __iter__(self):
         return iter(self._container)
